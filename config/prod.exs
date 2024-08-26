340581ec85c716823cf09720b49ffbe8fef67198
Ryan Schuetzler <ryan@schuetzler.net>
2024-08-26 13:01:34 -0600

    Add deploy fly.toml and domain

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :live_feedback, LiveFeedbackWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  url: [host: "live.chattr.io", port: 443]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: LiveFeedback.Finch
