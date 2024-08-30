ba52928f0dba57d5892dcd782dab2e674ad1e542
Ryan Schuetzler <ryan@schuetzler.net>
2024-08-30 10:52:00 -0600

    Add RBAC (#3)
    * Add RBAC to feedback page management
    
    * Add logo/favicon

--- lib/live_feedback_web/live/course_page_live/index.ex
+++ lib/live_feedback_web/live/course_page_live/index.ex
@@ -24,9 +24,24 @@ defmodule LiveFeedbackWeb.CoursePageLive.Index do
   end
 
   defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Feedback page")
-    |> assign(:course_page, Courses.get_course_page!(id))
+    course_page = Courses.get_course_page!(id)
+
+    with :ok <-
+           Bodyguard.permit(
+             Courses,
+             :change_course_page,
+             socket.assigns.current_user,
+             course_page
+           ) do
+      socket
+      |> assign(:page_title, "Edit Feedback page")
+      |> assign(:course_page, course_page)
+    else
+      {:error, _reason} ->
+        socket
+        |> put_flash(:error, "You are not authorized to manage this feedback page")
+        |> redirect(to: ~p"/course_pages")
+    end
   end
 
   defp apply_action(socket, :new, _params) do
@@ -49,8 +64,21 @@ defmodule LiveFeedbackWeb.CoursePageLive.Index do
   @impl true
   def handle_event("delete", %{"id" => id}, socket) do
     course_page = Courses.get_course_page!(id)
-    {:ok, _} = Courses.delete_course_page(course_page)
 
-    {:noreply, stream_delete(socket, :course_pages, course_page)}
+    with :ok <-
+           Bodyguard.permit(
+             Courses,
+             :delete_course_page,
+             socket.assigns.current_user,
+             course_page
+           ) do
+      {:ok, _} = Courses.delete_course_page(course_page)
+
+      {:noreply, stream_delete(socket, :course_pages, course_page)}
+    else
+      {:error, _reason} ->
+        socket
+        |> put_flash(:error, "You are not authorized to delete this page")
+    end
   end
 end
